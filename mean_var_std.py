6e17edc15ad9ceacb11dc8067c7e37c263a6d217
loikey1 <bui.chiloi98@gmail.com>
2024-12-04 22:49:50 +0700

    First commit

--- mean_var_std.py
+++ mean_var_std.py
@@ -6,16 +6,17 @@ def calculate(list):
         raise ValueError('List must contain nine numbers.')
     else:
         matrix = np.array(list).reshape(3,3)
-    mean = [(matrix.mean(axis=0).tolist()),(matrix.mean(axis=1).tolist()),(matrix.flatten().mean())]
-    var = [(matrix.var(axis=0).tolist()),(matrix.var(axis=1).tolist()),(matrix.flatten().var())]
-    std = [(matrix.std(axis=0).tolist()),(matrix.std(axis=1).tolist()),(matrix.flatten().std())]
-    max = [(matrix.max(axis=0).tolist()),(matrix.max(axis=1).tolist()),(matrix.flatten().max())]
-    min = [(matrix.min(axis=0).tolist()),(matrix.min(axis=1).tolist()),(matrix.flatten().min())]
-    sum = [(matrix.sum(axis=0).tolist()),(matrix.sum(axis=1).tolist()),(matrix.flatten().sum())]
+    mean = [matrix.mean(axis=0).tolist(), matrix.mean(axis=1).tolist(), float(matrix.mean())]
+    var = [matrix.var(axis=0).tolist(), matrix.var(axis=1).tolist(), float(matrix.var())]
+    std = [matrix.std(axis=0).tolist(), matrix.std(axis=1).tolist(), float(matrix.std())]
+    max = [matrix.max(axis=0).tolist(), matrix.max(axis=1).tolist(), float(matrix.max())]
+    min = [matrix.min(axis=0).tolist(), matrix.min(axis=1).tolist(), float(matrix.min())]
+    sum = [matrix.sum(axis=0).tolist(), matrix.sum(axis=1).tolist(), float(matrix.sum())]
+   
     calculate = {
         "mean": mean,
         "variance": var,
-        "stand deviation": std,
+        "standard deviation": std,
         "max": max,
         "min": min,
         "sum": sum,
